3e950d59397c7ab516ccb1fa563320a04b2777eb
Ermerson Gomes <ermersonwwe@gmail.com>
2024-12-02 11:35:17 -0300

    new update
    Foi  ensinado novos assuntos de javascript 02/12/24

--- javascript/form01.js
+++ javascript/form01.js
@@ -1,3 +1,19 @@
-function enviar(){
-    window.alert('Dados enviados com sucesso!');
-}   
+function destacar(ele){
+    ele.style.background = "#aaa"
+}
+function normal(ele){
+    ele.style.background = "white"
+}
+function funcao01(){
+    let validado = false;
+    let stremail = document.getElementById("email").value;
+
+    validado = stremail.includes('@');
+
+    if(validado){
+        window.alert('Formulario enviado com sucesso!')
+    }else{
+        window.alert('Digite um e-mail válido.')
+    }
+
+}
